62519dadb9613358fa8513c6aed666f07cccc564
yeasin2002 <yeasin2002.pro@gmail.com>
2026-09-04 00:58:28 +0600

    feat: initialize project workspace with server and database packages including user authentication schema

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,12 +1,12 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { env } from "@webbriks-technical-assessment/env/server";
 
-import { PrismaClient } from "../prisma/generated/client";
+import { PrismaClient } from "@prisma/client";
 
 const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
 
-export { prisma, PrismaClient };
-export type * from "../prisma/generated/client";
+export type * from "@prisma/client";
+export { PrismaClient, prisma };
 export default prisma;
 
